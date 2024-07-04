1488489497221d80f98ef41eba6785d063883613
sorocode <tudgus010707@naver.com>
2024-07-05 00:37:25 +0900

    chore: Update Zustand middleware in store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { create } from "zustand";
-
+import { devtools } from "zustand/middleware";
 interface NavState {
   navNum: number;
   setNavNum: (value: number) => void;
 }
 
-export const useNavStore = create<NavState>((set) => ({
-  navNum: 0,
-  setNavNum: (value: number) => set({ navNum: value }),
-}));
+export const useNavStore = create(
+  devtools<NavState>((set) => ({
+    navNum: 0,
+    setNavNum: (value: number) => set({ navNum: value }),
+  }))
+);
