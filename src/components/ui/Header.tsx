28b760bfa2e939b7f89abaddd2bba920a12e1cfe
sorocode <tudgus010707@naver.com>
2024-07-07 00:10:28 +0900

    💄  Update Header component layout and add login button

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,10 +1,18 @@
+import { AccountCircle } from "@mui/icons-material";
 import React, { FC } from "react";
-
+import { Link } from "react-router-dom";
 const Header: FC = () => {
   return (
-    <div className="flex items-center gap-2 bg-cyan-500 px-4 py-2">
-      <img src="/logo.png" alt="logo" width={50} />
-      <span>LetsTravel</span>
+    <div className="flex justify-between items-center bg-cyan-500 px-6 py-2">
+      <div className="flex items-center gap-2">
+        <Link to="/">
+          <img src="/logo.png" alt="logo" width={50} />
+        </Link>
+        <span>LetsTravel</span>
+      </div>
+      <Link to="/login">
+        <AccountCircle color="action" fontSize="large" />
+      </Link>
     </div>
   );
 };
