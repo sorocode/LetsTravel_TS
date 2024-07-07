5697b98d12ca39634b4f00f80fc766837b3fa204
sorocode <tudgus010707@naver.com>
2024-07-07 12:10:13 +0900

    refactor: Update user interface property names in user.d.ts

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,10 +1,10 @@
 export interface User {
-  user_seq: number;
+  memSeq: number;
   email: string;
   password: string;
   fname: string;
   lname: string;
-  nickname: string;
+  nickName: string;
   birthdate: string; // Date 타입도 가능하지만, 일반적으로 문자열로 처리함
   sex: number; // 0: Unknown, 1: Male, 2: Female
   regtime?: string; // Optional 필드
