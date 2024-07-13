9a9b7b37d520432d38f463284cd28e6c9487bebf
sorocode <tudgus010707@naver.com>
2024-07-13 22:57:43 +0900

    chore: Update calendar styling for consistency and accessibility

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -49,7 +49,7 @@ function Calendar({
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
-          "aria-selected:bg- aria-selected:text-accent-foreground",
+          "aria-selected:bg-sub aria-selected:text-accent-background",
         day_hidden: "invisible",
         ...classNames,
       }}
