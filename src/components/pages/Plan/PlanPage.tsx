5481daece853987b04c0b94fe91f6f6fbf84c90f
sorocode <tudgus010707@naver.com>
2024-07-13 21:30:27 +0900

    💄 플랜페이지 추가

--- src/components/pages/Plan/PlanPage.tsx
+++ src/components/pages/Plan/PlanPage.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import PlanArea from "@/components/ui/Plan/PlanArea";
+import RecommendArea from "@/components/ui/Plan/RecommendArea";
 import { FunctionComponent } from "react";
 import { useLocation, useParams } from "react-router-dom";
 interface PlanPageProps {}
@@ -8,9 +10,16 @@ const PlanPage: FunctionComponent<PlanPageProps> = () => {
   const location = useLocation();
   const cityInfo = { ...location.state };
   return (
-    <>
-      <PlanArea cityInfo={cityInfo} />
-    </>
+    <div className="flex flex-col h-3/4 justify-between">
+      <div>
+        <PlanArea cityInfo={cityInfo} />
+        <RecommendArea cityInfo={cityInfo} />
+      </div>
+      {/* TODO:db에 일정 저장 */}
+      <div className="flex flex-col items-center justify-end">
+        <Button variant="outline">일정 저장하고 계속하기</Button>
+      </div>
+    </div>
   );
 };
 
