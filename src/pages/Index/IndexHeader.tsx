68c255f4c83e59b5e451508f6cd3a7415eddeba7
sorocode <tudgus010707@naver.com>
2024-07-26 14:22:08 +0900

    [chore]로그인 상태에 따른 동적 컨텐츠

--- src/pages/Index/IndexHeader.tsx
+++ src/pages/Index/IndexHeader.tsx
@@ -1,26 +1,33 @@
-import { MapIcon, Menu } from "lucide-react";
-import { FunctionComponent } from "react";
-import { Link } from "react-router-dom";
+import {MapIcon, Menu} from "lucide-react";
+import {FunctionComponent} from "react";
+import {Link} from "react-router-dom";
+import {useLoginStore} from "@/store/store"
 
-interface IndexHeaderProps {}
+interface IndexHeaderProps {
+}
 
 const IndexHeader: FunctionComponent<IndexHeaderProps> = () => {
-  return (
-    <div className="flex justify-between px-1 mb-3">
-      <div className="flex items-center gap-2">
-        <img src="profileCircle.svg" alt="profile image" width="30%" />
-        <span>소로소님</span>
-      </div>
-      <div className="flex justify-center items-center gap-2">
-        <Link to="map">
-          <MapIcon />
-        </Link>
-        <Link to="#">
-          <Menu />
-        </Link>
-      </div>
-    </div>
-  );
+    const isLogin = useLoginStore((state) => state.isLogin);
+
+    return (
+        <div className="flex justify-between px-1 mb-3">
+            <div className="flex items-center gap-2">
+                <img src="profileCircle.svg" alt="profile image" width="30%"/>
+                <div>
+                    {isLogin && <span>소로소님</span>}
+                    {!isLogin && <Link to="login">로그인</Link>}
+                </div>
+            </div>
+            <div className="flex justify-center items-center gap-2">
+                <Link to="map">
+                    <MapIcon/>
+                </Link>
+                <Link to="#">
+                    <Menu/>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default IndexHeader;
