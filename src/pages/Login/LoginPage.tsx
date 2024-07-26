0ce14200aa16a2c52179e41b79eb76c61fb0eae1
sorocode <tudgus010707@naver.com>
2024-07-26 15:38:53 +0900

    feat: Add Header component to LoginPage
    The Header component is added to the LoginPage to provide a consistent header across the application.

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -4,27 +4,31 @@ import { FC } from "react";
 import { Separator } from "@/components/ui/separator";
 
 import LoginForm from "./LoginForm";
+import Header from "@/components/ui/common/Header";
 
 interface LoginProps {}
 
 const LoginPage: FC<LoginProps> = () => {
   return (
-    <div className="flex flex-col gap-6 mt-10">
+    <>
+      <Header />
+      <div className="flex flex-col gap-6 mt-8 px-1">
         <p className="font-medium text-lg">로그인</p>
-      <LoginForm />
-      <div className="flex justify-center items-center gap-2">
-        <Button type="button" variant="link">
-          아이디찾기
-        </Button>
-        <Separator orientation="vertical" className="h-5 bg-gray-400" />
-        <Button type="button" variant="link">
-          비밀번호 찾기
+        <LoginForm />
+        <div className="flex justify-center items-center gap-2">
+          <Button type="button" variant="link">
+            아이디찾기
+          </Button>
+          <Separator orientation="vertical" className="h-5 bg-gray-400" />
+          <Button type="button" variant="link">
+            비밀번호 찾기
+          </Button>
+        </div>
+        <Button asChild variant="outline">
+          <Link to="/signup">회원가입</Link>
         </Button>
       </div>
-      <Button asChild variant="outline">
-        <Link to="/signup">회원가입</Link>
-      </Button>
-    </div>
+    </>
   );
 };
 
